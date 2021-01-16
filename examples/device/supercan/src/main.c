ef4c4afb7cd8dcf7fc654e8be944f1246a1ba667
Jean Gressmann <jean@0x42.de>
2021-01-16 05:48:21 +0100

    SuperCAN: fix logging of computed CAN bit time

--- examples/device/supercan/src/main.c
+++ examples/device/supercan/src/main.c
@@ -263,7 +263,7 @@ static inline void can_log_nominal_bit_timing(struct can *c)
 	LOG("nominal brp=%u sjw=%u tseg1=%u tseg2=%u bitrate=%lu sp=%u/1000\n",
 		c->nmbt_brp, c->nmbt_sjw, c->nmbt_tseg1, c->nmbt_tseg2,
 		CAN_CLK_HZ / ((uint32_t)c->nmbt_brp * (1 + c->nmbt_tseg1 + c->nmbt_tseg2)),
-		((c->nmbt_tseg1) * 1000) / (c->nmbt_tseg1 + c->nmbt_tseg2)
+		((1 + c->nmbt_tseg1) * 1000) / (1 + c->nmbt_tseg1 + c->nmbt_tseg2)
 	);
 }
 
@@ -274,7 +274,7 @@ static inline void can_log_data_bit_timing(struct can *c)
 	LOG("data brp=%u sjw=%u tseg1=%u tseg2=%u bitrate=%lu sp=%u/1000\n",
 		c->dtbt_brp, c->dtbt_sjw, c->dtbt_tseg1, c->dtbt_tseg2,
 		CAN_CLK_HZ / ((uint32_t)c->dtbt_brp * (1 + c->dtbt_tseg1 + c->dtbt_tseg2)),
-		((c->dtbt_tseg1) * 1000) / (c->dtbt_tseg1 + c->dtbt_tseg2)
+		((1 + c->dtbt_tseg1) * 1000) / (1 + c->dtbt_tseg1 + c->dtbt_tseg2)
 	);
 }
 
