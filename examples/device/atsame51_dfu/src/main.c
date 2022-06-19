41efa1784cf331ae6dec1632549ec7ac63193225
Jean Gressmann <jean@0x42.de>
2022-06-19 21:25:00 +0200

    SuperDFU: re-enable UART prints

--- examples/device/atsame51_dfu/src/main.c
+++ examples/device/atsame51_dfu/src/main.c
@@ -536,14 +536,14 @@ int main(void)
 		WDT->CONFIG.bit.PER = per;
 		WDT->CTRLA.bit.ENABLE = 1;
 
-		// board_uart_write(NAME " gl hf, starting ", -1);
-		// board_uart_write(dfu_app_tag_ptr->app_name, -1);
-		// board_uart_write("...\n", -1);
+		board_uart_write(NAME " gl hf, starting ", -1);
+		board_uart_write(dfu_app_tag_ptr->app_name, -1);
+		board_uart_write("...\n", -1);
 
 		// go go go
 		start_app_jump(SUPERDFU_BOOTLOADER_SIZE);
 	} else {
-		// board_uart_write(NAME " v" STR(SUPERDFU_VERSION_MAJOR) "." STR(SUPERDFU_VERSION_MINOR) "." STR(SUPERDFU_VERSION_PATCH) " running\n", -1);
+		board_uart_write(NAME " v" STR(SUPERDFU_VERSION_MAJOR) "." STR(SUPERDFU_VERSION_MINOR) "." STR(SUPERDFU_VERSION_PATCH) " running\n", -1);
 
 		// set app stable counter
 		dfu_hdr_ptr()->counter = 0;
