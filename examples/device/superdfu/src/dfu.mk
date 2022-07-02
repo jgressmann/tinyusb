7f442e4b3a776869624ffd60b449caf1a52929d1
Jean Gressmann <jean@0x42.de>
2022-07-02 08:36:52 +0200

    SuperDFU: fixes path the patch script

--- examples/device/superdfu/src/dfu.mk
+++ examples/device/superdfu/src/dfu.mk
@@ -13,7 +13,7 @@ DFU_DFU_FILE=$(OUT_BASE_NAME).dfu
 $(BUILD)/$(DFUED_ELF_FILE): $(BUILD)/$(ELF_FILE)
 	@echo CREATE $@
 	@$(CP) $^ $@.tmp
-	$(PYTHON) $(TOP)/examples/device/atsame51_dfu/src/superdfu-patch.py --strict 1 --tag $(BUILD)/$(DFU_TAG_FILE) $@.tmp $(shell $(OBJDUMP) -t $^ | grep _sfixed | head -n 1 | awk '{print $$1;}') $(shell $(OBJDUMP) -t $^ | grep dfutag | head -n 1 | awk '{print $$1;}')
+	$(PYTHON) $(TOP)/examples/device/superdfu/src/superdfu-patch.py --strict 1 --tag $(BUILD)/$(DFU_TAG_FILE) $@.tmp $(shell $(OBJDUMP) -t $^ | grep _sfixed | head -n 1 | awk '{print $$1;}') $(shell $(OBJDUMP) -t $^ | grep dfutag | head -n 1 | awk '{print $$1;}')
 	@mv $@.tmp $@
 
 $(BUILD)/$(DFUED_HEX_FILE): $(BUILD)/$(DFUED_ELF_FILE)
@@ -23,7 +23,7 @@ $(BUILD)/$(DFUED_HEX_FILE): $(BUILD)/$(DFUED_ELF_FILE)
 $(BUILD)/$(DFUED_BIN_FILE): $(BUILD)/$(DFUED_ELF_FILE)
 	@echo CREATE $@
 	@$(OBJCOPY) -O binary $^ $@
-	#$(PYTHON) $(TOP)/examples/device/atsame51_dfu/src/crc32.py 0 $(shell $(OBJDUMP) -t $^ | grep dfutag | head -n 1 | awk '{print $$1;}') $@
+	#$(PYTHON) $(TOP)/examples/device/superdfu/src/crc32.py 0 $(shell $(OBJDUMP) -t $^ | grep dfutag | head -n 1 | awk '{print $$1;}') $@
 
 flash-dfu: $(BUILD)/$(DFUED_HEX_FILE)
 	@echo halt > $(BUILD)/$(BOARD).superdfu.jlink
